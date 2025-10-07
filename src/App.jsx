eb3ce8378a91b0b0312e29a687cac6b486da015a
amanmaurya005 <amaurya9465@gmail.com>
2025-10-07 18:12:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import Aman from './components/Aman'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import FoundNot from "./pages/FoundNot"
+import {FoundNot} from "./pages/FoundNot"
 
 
 const router = createBrowserRouter([
