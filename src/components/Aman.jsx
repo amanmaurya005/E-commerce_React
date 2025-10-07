85b40722142be401330a9e7a507b12b9c7e20991
amanmaurya005 <amaurya9465@gmail.com>
2025-10-07 17:25:12 +0530

    updated

--- src/components/Aman.jsx
+++ src/components/Aman.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainSection from '../pages/MainSection'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
-import Footer from './footer'
+import Footer from './Footer'
 
 export default function Aman() {
     return (
